003df633e8c502273985464f48db9a34498f1cf5
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-04-01 11:44:13 -0300

    Project created

---
+++ backend/src/main/java/com/gtecnologia/GTcontrole/GTcontroleApplication.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.GTcontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GTcontroleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTcontroleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gtecnologia/GTcontrole/GTcontroleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.GTcontrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GTcontroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
