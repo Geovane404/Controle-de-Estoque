a00e9cda519be78b8a7bdd0285fd2d274970ffa8
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-04-03 16:47:37 -0300

    FindAllPaged and findById

--- backend/src/main/java/com/gtecnologia/GTcontrole/controllers/CategoryController.java
+++ backend/src/main/java/com/gtecnologia/GTcontrole/controllers/CategoryController.java
@@ -3,13 +3,15 @@ package com.gtecnologia.GTcontrole.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gtecnologia.GTcontrole.dto.CategoryDTO;
-import com.gtecnologia.GTcontrole.entities.Category;
 import com.gtecnologia.GTcontrole.services.CategoryService;
 
 @RestController
@@ -23,8 +25,21 @@ public class CategoryController {
 	public ResponseEntity<List<CategoryDTO>> findAll (){
 		
 		List<CategoryDTO> list = service.findAll();
-		
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/paged")
+	public ResponseEntity<Page<CategoryDTO>> findAllPaged(Pageable pageable) {
+		
+		Page<CategoryDTO> pagelist = service.findALLPaged(pageable);
+		return ResponseEntity.ok().body(pagelist);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
 }
+
--- backend/src/main/java/com/gtecnologia/GTcontrole/services/CategoryService.java
+++ backend/src/main/java/com/gtecnologia/GTcontrole/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,8 +23,20 @@ public class CategoryService {
 	public List<CategoryDTO> findAll () {
 		
 		List<Category> list  =  repository.findAll();
-		 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findALLPaged(Pageable pageable) {
+		
+		Page<Category>pagelist = repository.findAll(pageable);
+		return pagelist.map(x -> new CategoryDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		
+		Category entity = repository.findById(id).get();
+		return new CategoryDTO(entity);
+	}
 }
