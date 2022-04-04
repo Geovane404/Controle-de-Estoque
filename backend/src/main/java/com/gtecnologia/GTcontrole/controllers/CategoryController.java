88b8a9d933c79cf235d2ff9d8c7eb365fce2790d
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-04-04 10:11:38 -0300

    Created new Category

--- backend/src/main/java/com/gtecnologia/GTcontrole/controllers/CategoryController.java
+++ backend/src/main/java/com/gtecnologia/GTcontrole/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package com.gtecnologia.GTcontrole.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 
 import com.gtecnologia.GTcontrole.dto.CategoryDTO;
 import com.gtecnologia.GTcontrole.services.CategoryService;
@@ -41,5 +46,14 @@ public class CategoryController {
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
-
--- backend/src/main/java/com/gtecnologia/GTcontrole/services/CategoryService.java
+++ backend/src/main/java/com/gtecnologia/GTcontrole/services/CategoryService.java
@@ -43,4 +43,15 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado!"));
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		
+		Category entity = new Category();
+	
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
 }
