a1a128bd315ef4d51c722d95b304db5985e1a62f
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-04-06 08:11:55 -0300

    Auditing data

--- backend/src/main/java/com/gtecnologia/GTcontrole/entities/Category.java
+++ backend/src/main/java/com/gtecnologia/GTcontrole/entities/Category.java
@@ -1,11 +1,15 @@
 package com.gtecnologia.GTcontrole.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +22,13 @@ public class Category implements Serializable{
 	private Long id;
 	
 	private String name;
+	
+	//Dados-Auditorias(criação e atualizações)
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 
 	public Category() {
 
@@ -42,6 +53,25 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	//Dados-Auditorias(criação e atualizações)
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
