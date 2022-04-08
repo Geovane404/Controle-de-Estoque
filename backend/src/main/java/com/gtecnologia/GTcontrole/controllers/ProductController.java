b229490383ad1149b0fd44469d5c14e7f4210fd5
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-04-08 09:17:26 -0300

    Product Insert

--- backend/src/main/java/com/gtecnologia/GTcontrole/controllers/ProductController.java
+++ backend/src/main/java/com/gtecnologia/GTcontrole/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.gtecnologia.GTcontrole.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gtecnologia.GTcontrole.dto.ProductDTO;
 import com.gtecnologia.GTcontrole.services.ProductService;
@@ -41,5 +45,13 @@ public class ProductController {
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- backend/src/main/java/com/gtecnologia/GTcontrole/services/ProductService.java
+++ backend/src/main/java/com/gtecnologia/GTcontrole/services/ProductService.java
@@ -10,8 +10,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gtecnologia.GTcontrole.dto.CategoryDTO;
 import com.gtecnologia.GTcontrole.dto.ProductDTO;
+import com.gtecnologia.GTcontrole.entities.Category;
 import com.gtecnologia.GTcontrole.entities.Product;
+import com.gtecnologia.GTcontrole.repositories.CategoryRepository;
 import com.gtecnologia.GTcontrole.repositories.ProductRepository;
 import com.gtecnologia.GTcontrole.services.exception.ResourceNotFoundException;
 
@@ -20,6 +23,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public List<ProductDTO> findAll() {
@@ -43,5 +49,34 @@ public class ProductService {
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado!"));
 		return new ProductDTO(entity);
 	}
+
+	@Transactional
+	public ProductDTO insert( ProductDTO dto) {
+		
+		Product entity = new Product();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
+	}
+
 	
+	//-----------METODOS AUXILIARES------------------------------//
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+	
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setDate(dto.getDate());
+		
+		entity.getCategories().clear();
+		
+		for(CategoryDTO catDto : dto.getCategories()) {
+			
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+	}
+
 }
